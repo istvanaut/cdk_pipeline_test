cd82a8d4bfdfb2a159246170d92c2a3a8fb1dc26
Istvan Fabian <Istvan.Fabian@logmein.com>
2022-07-07 15:23:30 +0200

    Added basic pipeline code

--- lib/cdk_pipeline_test-stack.ts
+++ lib/cdk_pipeline_test-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
+import {ManualApprovalStep} from "aws-cdk-lib/pipelines";
 
 export class CdkPipelineTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyTestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('istvanaut/cdk_pipeline_test', 'main'),
+        commands: ['npm ci','npm run build','npx cdk synth']
+      })
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelineTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
